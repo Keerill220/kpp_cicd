777aecfa60f3edb73eac6d73e59b3a64ec17d1d9
Keerill220 <kponomarenko161@gmail.com>
2025-12-12 14:06:44 +0200

    renamed the app, added ugly ass icons, fixed the version numbers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,14 +23,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.water_tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        
+        // Use Bitrise build number if available, otherwise use Flutter's
+        val bitriseNumber = System.getenv("BITRISE_BUILD_NUMBER")?.toIntOrNull()
+        versionCode = bitriseNumber ?: flutter.versionCode
         versionName = flutter.versionName
+        
         multiDexEnabled = true
     }
 
